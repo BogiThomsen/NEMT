a154b73d5dfbc322bf1bc437e7bbb59d35f35c51
Bogi <bogi1511@gmail.com>
2018-12-04 14:48:32 +0100

    minor update

---
+++ services/data_access/user/app/user_api.py
@@ -0,0 +1,14 @@
+from flask import jsonify, request
+
+users = []
+
+def addUser():
+    user = {
+        'username': request.json['username'],
+        'email': request.json['email']
+    }
+    users.append(user)
+    return jsonify({'user': user}), 201
+
+def getUser():
+    return jsonify(users)

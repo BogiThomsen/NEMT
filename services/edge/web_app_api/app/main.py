9518d0e317642d5e7f577a7dcbd1b848e92b998c
Jens Burla Johansen <jbjo15@student.aau.dk>
2018-12-05 12:13:27 +0100

    Added layout template and template inheritance as well as login_manager

--- services/edge/web_app_api/app/main.py
+++ services/edge/web_app_api/app/main.py
@@ -1,7 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import flask_login
+import login_manager
 import requests
 import sys
+
 app = Flask(__name__)
+app.secret_key = '&0n2%~pq0B=j8TS('
+
+login_manager.login_manager.init_app(app)
+
+users = {'burla': {'password': '1234'}}
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='username' id='username' placeholder='username'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    username = request.form['username']
+    if request.form['password'] == users[username]['password']:
+        user = login_manager.User()
+        user.id = username
+        flask_login.login_user(user)
+        return redirect(url_for('protected'))
+
+    return 'Bad login'
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
 
 @app.route('/')
 def index():

6ceabb6f4c5b2d512cc31764aa231944e1f75ff5
Burla <jens@burla.dk>
2018-12-05 16:03:25 +0100

    Added versioning to API endpoints and prepared response codes

--- services/edge/web_app_api/app.py
+++ services/edge/web_app_api/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 import requests
 
 app = Flask(__name__)
 
-@app.route('/users', methods=['POST'])
+# Make use of the following response codes
+# return make_response('Ok', 200)
+# return make_response('Created', 201)
+# return make_response('Bad Request', 400)
+# return make_response('Unauthorized', 401)
+# return make_response('Forbidden', 403)
+# return make_response('Not Found', 404)
+# return make_response('Internal Server Error', 500)
+# return make_response('Service Unavailable', 503)
+
+@app.route('/v1/users', methods=['POST'])
 def users():
     # Redirect to Core service
     pass
 
-@app.route('/users/<string:id>', methods=['GET', 'PUT'])
+@app.route('/v1/users/<string:id>', methods=['GET', 'PUT'])
 def users_id(id):
     if request.method == 'GET':
         # Redirect to Core service
@@ -17,17 +27,17 @@ def users_id(id):
         # Redirect to Core service
         pass
 
-@app.route('/users/<string:id>/availableDevices', methods=['GET'])
-def users_available_devices(id):
+@app.route('/v1/users/<string:id>/availableDevices', methods=['GET'])
+def users_id_available_devices(id):
     # Redirect to Core service
     pass
 
-@app.route('/users/<string:id>/rules', methods=['GET'])
-def users_rules(id):
+@app.route('/v1/users/<string:id>/rules', methods=['GET'])
+def users_id_rules(id):
     # Redirect to Core service
     pass
 
-@app.route('/devices', methods=['GET', 'POST'])
+@app.route('/v1/devices', methods=['GET', 'POST'])
 def devices():
     if request.method == 'GET':
         # Query parameter page=:p used for pagination
@@ -37,7 +47,7 @@ def devices():
         # Redirect to Core service
         pass
 
-@app.route('/devices/<string:id>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/v1/devices/<string:id>', methods=['GET', 'PUT', 'DELETE'])
 def devices_id(id):
     if request.method == 'GET':
         # Redirect to Core service
@@ -49,17 +59,17 @@ def devices_id(id):
         # Redirect to Core service
         pass
 
-@app.route('/devices/<string:id>/rules', methods=['GET'])
-def devices_id(id):
+@app.route('/v1/devices/<string:id>/rules', methods=['GET'])
+def devices_id_rules(id):
     # Redirect to Core service
     pass
 
-@app.route('/rules', methods=['POST'])
+@app.route('/v1/rules', methods=['POST'])
 def rules():
     # Redirect to Core service
     pass
 
-@app.route('/rules/<string:id>', methods=['GET', 'PUT', 'DELETE'])
+@app.route('/v1/rules/<string:id>', methods=['GET', 'PUT', 'DELETE'])
 def rules_id(id):
     if request.method == 'GET':
         # Redirect to Core service
@@ -72,4 +82,4 @@ def rules_id(id):
         pass
 
 if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True)

410493a3f29125774f80b88c16c205b3848d04b8
Unknown <andreascml@gmail.com>
2018-12-05 14:07:28 +0100

    Made data_accesses into classes
    Made data_accesses into classes, added functionality for adding/removing devices,rules and groups

--- services/data_access/user/app/user_data_access.py
+++ services/data_access/user/app/user_data_access.py
@@ -1,54 +1,44 @@
 import pymongo
 from bson.objectid import ObjectId
 
-client = pymongo.MongoClient("mongodb+srv://Andreas:dummypassword64@sw7-3mptj.gcp.mongodb.net/admin")
-db = client["database"]
+class UserAccessLayer(object):
+    def __init__(self):
+        client = pymongo.MongoClient("mongodb+srv://Andreas:dummypassword64@sw7-3mptj.gcp.mongodb.net/admin")
+        db = client["database"]
+        self.user_db = db["Users"]
 
-user_db = db["Users"]
+    def add_user(self, username, password, access_token):
+        new_user = {"username": username, "password": password, "access_token": access_token}
+        self.user_db.insert_one(new_user)
 
-def add_user(username, password, access_token):
-    new_user = {"username": username, "password": password, "access_token": access_token}
-    user_db.insert_one(new_user)
+    def remove_user(self, user_id):
+        query = {"_id": ObjectId(user_id)}
+        self.user_db.delete_one(query)
 
-def remove_user(user_id):
-    query = {"_id": ObjectId(user_id)}
-    user_db.delete_one(query)
+    def change_username(self, user_id, new_username):
+        self.user_db.update_one({"_id": ObjectId(user_id)},
+                             { "$set": { "username": new_username}})
 
-def change_username(user_id, new_username):
-    user_db.update_one({"_id": ObjectId(user_id)},
-                         { "$set": { "username": new_username}})
+    def change_password(self, user_id, new_password):
+        self.user_db.update_one({"_id": ObjectId(user_id)},
+                             { "$set": { "password": new_password}})
 
-def change_password(user_id, new_password):
-    user_db.update_one({"_id": ObjectId(user_id)},
-                         { "$set": { "password": new_password}})
+    def get_user(self, user_id):
+        x = self.user_db.find_one({"_id": ObjectId(user_id)})
+        x["_id"] = str(x["_id"])
+        return x
 
-def get_user(user_id):
-    x = user_db.find_one({"_id": ObjectId(user_id)})
-    x["_id"] = str(x["_id"])
-    return x
-
-def get_id_by_username(username):
-    x = user_db.find_one({"username": username})
-    return str(x["_id"])
-
-
-#def add_device_to_user(user_id)
+    def get_id_by_username(self, username):
+        x = self.user_db.find_one({"username": username})
+        return str(x["_id"])
 
 
+    def add_to_user(self, user_id, id_to_add, where_to_add):
+        self.user_db.update_one({"_id": ObjectId(user_id)},
+                       { "$push":  {where_to_add: id_to_add} })
+
+    def remove_from_user(self, user_id, id_to_remove, Where_to_remove):
+        self.user_db.update_one({"_id": ObjectId(user_id)},
+                            {"$pull": {Where_to_remove: id_to_remove} })
 
 
-#Testies
-#add_user("LarsAllan", "1234567", "Second_Breakfast47")
-
-##remember to find id
-#id_to_remove = "dummyid"
-#remove_user(id_to_remove)
-
-id_to_change_name = "5c067e0f6025271da8e45fab"
-#change_username(id_to_change_name, "Changed Username")
-
-#change_password(id_to_change_name, "newPassword")
-print(get_user(id_to_change_name))
-print(get_user("5c0678f36025271d7857066b"))
-
-print(get_id_by_username("LarsAllan"))

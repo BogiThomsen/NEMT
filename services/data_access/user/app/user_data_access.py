8ac0b0cd4162c0dd33c77da646b2fcc97bc91872
Unknown <andreascml@gmail.com>
2018-12-06 16:30:27 +0100

    fixed my double work

--- services/data_access/user/app/user_data_access.py
+++ services/data_access/user/app/user_data_access.py
@@ -1,60 +1,51 @@
 import pymongo
 from bson.objectid import ObjectId
-from flask import Flask, request, make_response
-
-app = Flask(__name__)
 
 def connect_to_db():
     return pymongo.MongoClient("mongodb+srv://Andreas:dummypassword64@sw7-3mptj.gcp.mongodb.net/admin")["datebase"]["Users"]
 
-@app.route('/users', methods=['POST'])
-def post_user():
-    json = request.args
+
+def post_user(username, password, access_token):
     user_db = connect_to_db()
-    msg = user_db.insert_one(json)
-    if msg:
-        return make_response('Error', 400)
-    else:
-        return make_response('Ok', 200)
+    new_user = {"username": username, "password": password, "access_token": access_token}
+    user_db.insert_one(new_user)
 
 
-@app.route('/users/<String:id>', methods=['DELETE'])
-def delete_user(id):
+def delete_user(user_id):
     user_db = connect_to_db()
-    query = {"_id": ObjectId(id)}
+    query = {"_id": ObjectId(user_id)}
     user_db.delete_one(query)
 
-@app.route('/')
+
 def put_username(user_id, new_username):
     user_db = connect_to_db()
     user_db.update_one({"_id": ObjectId(user_id)},
                          { "$set": { "username": new_username}})
 
-@app.route('/users/<String:id>', methods=['PUT'])
+
 def put_password(user_id, new_password):
     user_db = connect_to_db()
     user_db.update_one({"_id": ObjectId(user_id)},
                          { "$set": { "password": new_password}})
 
-@app.route('/')
+
 def get_user(user_id):
     user_db = connect_to_db()
     x = user_db.find_one({"_id": ObjectId(user_id)})
     x["_id"] = str(x["_id"])
     return x
 
-@app.route('/')
+
 def get_user_id_by_username(username):
     user_db = connect_to_db()
     x = user_db.find_one({"username": username})
     return str(x["_id"])
 
-@app.route('/')
 def post_to_user(user_id, id_to_add, where_to_add):
     user_db = connect_to_db()
     user_db.update_one({"_id": ObjectId(user_id)},
                    { "$push":  {where_to_add: id_to_add} })
-@app.route('/')
+
 def delete_from_user(user_id, id_to_remove, Where_to_remove):
     user_db = connect_to_db()
     user_db.update_one({"_id": ObjectId(user_id)},

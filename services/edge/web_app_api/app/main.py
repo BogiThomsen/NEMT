42b8b2d512c507750de762df719cba94185f6908
Unknown <bastianlol@live.dk>
2018-12-06 08:58:28 +0100

    Updated template, made login pretty, commented out all Docker functionality
    Without docker, UI can now be served through flask only.

--- services/edge/web_app_api/app/main.py
+++ services/edge/web_app_api/app/main.py
@@ -11,25 +11,26 @@ login_manager.login_manager.init_app(app)
 
 users = {'burla': {'password': '1234'}}
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='username' id='username' placeholder='username'/>
-                <input type='password' name='password' id='password' placeholder='password'/>
-                <input type='submit' name='submit'/>
-               </form>
-               '''
+    #if request.method == 'GET':
+    #    return '''
+    #           <form action='login' method='POST'>
+    #            <input type='text' name='username' id='username' placeholder='username'/>
+    #            <input type='password' name='password' id='password' placeholder='password'/>
+    #            <input type='submit' name='submit'/>
+    #           </form>
+    #           '''
 
-    username = request.form['username']
-    if request.form['password'] == users[username]['password']:
-        user = login_manager.User()
-        user.id = username
-        flask_login.login_user(user)
-        return redirect(url_for('protected'))
+    if request.method == 'POST':
+        username = request.form['username']
+        if request.form['password'] == users[username]['password']:
+            user = login_manager.User()
+            user.id = username
+            flask_login.login_user(user)
+            return redirect(url_for('protected'))
 
-    return 'Bad login'
+    return render_template('auth/signin.html')
 
 @app.route('/logout')
 def logout():
@@ -43,17 +44,18 @@ def protected():
 
 @app.route('/')
 def index():
-    r = requests.get("http://172.19.0.1:8085/api/getUser")
-    return render_template('index.html', text=r.json())
+    #r = requests.get("http://172.19.0.1:8085/api/getUser")
+    #return render_template('index.html', text=r.json())
+    return 'Hello'
 
-@app.route('/addUser', methods=['GET', 'POST'])
-def addUser():
-    json = {}
-    json["username"] = "DRUNAR"
-    json["email"] = "e@mail.com"
-    r = requests.post("http://172.19.0.1:8085/api/addUser", json=json)
-    return render_template('index.html', text=r)
+#@app.route('/addUser', methods=['GET', 'POST'])
+#def addUser():
+#    json = {}
+#    json["username"] = "DRUNAR"
+#    json["email"] = "e@mail.com"
+#    r = requests.post("http://172.19.0.1:8085/api/addUser", json=json)
+#    return render_template('index.html', text=r)
 
 
 if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0')
+  app.run()

9cca0c1d0702cb2b7148d660c8ad6f754b64cee9
Jens Burla Johansen <jbjo15@student.aau.dk>
2018-12-06 08:55:04 +0100

    Removed core testing code irrelevant for ui

--- services/edge/web_app_api/app/main.py
+++ services/edge/web_app_api/app/main.py
@@ -43,17 +43,8 @@ def protected():
 
 @app.route('/')
 def index():
-    r = requests.get("http://172.19.0.1:8085/api/getUser")
-    return render_template('index.html', text=r.json())
-
-@app.route('/addUser', methods=['GET', 'POST'])
-def addUser():
-    json = {}
-    json["username"] = "DRUNAR"
-    json["email"] = "e@mail.com"
-    r = requests.post("http://172.19.0.1:8085/api/addUser", json=json)
-    return render_template('index.html', text=r)
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True)
+  

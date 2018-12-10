c936b8e4123d142025d477be895f5c68c9949f18
Christian Knudsen <cakn15@student.aau.dk>
2018-12-10 16:09:15 +0100

    Added hashing and small changes

--- services/core/user/app/main.py
+++ services/core/user/app/main.py
@@ -1,38 +1,39 @@
 from flask import Flask, request
 from services.data_access.user.app import user_data_access
-from services.data_access.user.app import device_data_access
+from services.data_access.device import device_data_access
 import json
+import hashlib
+import uuid
+import secrets
 import connexion
 
 #Setups that is used throughout the user service.
-app = Flask.__name__
+##app = connexion.App(__name__, specification_dir = './')
+app = Flask(__name__)
+app.add_api('swagger.yml')
 data_access = user_data_access.UserAccessLayer()
 device_access = device_data_access.DeviceAccessLayer()
-
 
 @app.route('/user/<String:username>')
 def getUser(username):
     user_json = request.get_json(username)
     user_object = json.loads(user_json)
     user_id = data_access.get_user_id_by_username(user_object.username)
-
     user = json.loads(data_access.get_user(user_id))
 
-    if(user.password != user_object.password):
-        return None
+    if checkPassword(user.password, user_object.password):
+        return
     else:
-        return user
+        return None
 
 
-@app.route('/user/String:user_id')
-def getUserDevices(user_id):
-    #Unpack json object
-    user_id_from_request = request.get_json(user_id)
-    #get json object of a user, using get_user from data access layer
-    user_json = data_access.get_user(user_id_from_request.user_id)
+@app.route('/user/String:username')
+def getUserDevices(username):
+    user = getUser(request.json(username))
+
     #Retrieve list of devices
     #user = x.get_user(x.get_user_id_by_username("default username")) print(user["available_devices"])
-    user = json.loads(user_json)
+    user = json.loads(user)
     user_devices = []
 
     for device_id in user.available_devices:
@@ -42,11 +43,37 @@ def getUserDevices(user_id):
 
 
 @app.route('/user/String:user')
-def updateUser(user):
-    user = json.loads(data_access.get_user(user))
-    data_access.patch(user)
+def patchUsername(user):
+    #user = json.loads(data_access.get_user(user_id))
+    return None
 
-    
+@app.route('/user/String:user')
+def patchUserPassword(user):
+    #user = json.loads(data_access.get_user(user_id))
+    return None
+
+def hashPassword(password):
+    foo = uuid.uuid4().hex
+    return hashlib.sha256(foo.encode() + password.encode()).hexdigest() + ':' + foo
+
+def checkPassword(hashedPassword, userPassword):
+    password, foo = hashedPassword.split(':')
+    return userPassword == hashlib.sha256(foo.encode() + password.encode()).hexdigest()
+
+@app.route('user/String:user')
+def postUser(user):
+    jsonuser = {
+        'username': request.json['username'],
+        'password': hashPassword(request.json['password']),
+        'access_token': secrets.token_urlsafe(20)
+    }
+
+    data_access.post_user(jsonuser)
+
+def deleteUser(user_id):
+    return data_access.deleteUser(user_id)
+
+
 
 if __name__ == "__main__":
-  app.run(debug=True,host='0.0.0.0',  port='5100')
+  app.run(debug=True,host='localhost')

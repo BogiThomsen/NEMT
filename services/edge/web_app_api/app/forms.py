f9011d6d4e562ffe300e54c98290a2a240bbdc05
Jens Burla Johansen <jbjo15@student.aau.dk>
2018-12-10 15:54:58 +0100

    Added usage of WTForms for easy form generation and validation

---
+++ services/edge/web_app_api/app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, EqualTo
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+    # Request user from core service based on username
+    # If username is already taken raise validation error:
+    # raise ValidationError('Username already taken. Please use another.')
+        pass
+
+class SignInForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign in')
+
+    # Check if user exists with the username and if the password is correct
+
--- services/edge/web_app_api/app/main.py
+++ services/edge/web_app_api/app/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import flask_login
 import login_manager
 import datetime
+from urllib.parse import urlparse
+from forms import SignInForm
 import requests
 import sys
 
@@ -9,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = '&0n2%~pq0B=j8TS('
 
 login_manager.login_manager.init_app(app)
-login_manager.login_manager.login_view = 'login'
+login_manager.login_manager.login_view = 'signin'
 
 users = {'burla': {'password': '1234'}}
 
@@ -19,24 +21,22 @@ def inject_now():
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    #if request.method == 'GET':
-    #    return '''
-    #           <form action='login' method='POST'>
-    #            <input type='text' name='username' id='username' placeholder='username'/>
-    #            <input type='password' name='password' id='password' placeholder='password'/>
-    #            <input type='submit' name='submit'/>
-    #           </form>
-    #           '''
+    if not flask_login.current_user.is_anonymous:
+        return redirect(url_for('index'))
 
-    if request.method == 'POST':
-        username = request.form['username']
-        if request.form['password'] == users[username]['password']:
+    form = SignInForm(request.form)
+    if form.validate_on_submit():
+        username = form.username.data
+        if form.password.data == users[username]['password']:
             user = login_manager.User()
             user.id = username
             flask_login.login_user(user)
-            return redirect(url_for('protected'))
+            next = request.args.get('next')
+            if not next or urlparse(next).netloc != '':
+                next = url_for('index')
+            return redirect(next)
 
-    return render_template('auth/signin.html')
+    return render_template('auth/signin.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -45,6 +45,7 @@ def register():
     return render_template('auth/register.html')
 
 @app.route('/signout')
+@flask_login.login_required
 def logout():
     flask_login.logout_user()
     return render_template('auth/logout.html')

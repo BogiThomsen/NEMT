396c51ff26f1017d36a5b0d0abfa48bc6d5d030f
Bogi <bogi1511@gmail.com>
2018-12-11 09:53:04 +0100

    updated DA path, and various other changes

--- services/edge/web_app_api/app/main.py
+++ services/edge/web_app_api/app/main.py
@@ -15,8 +15,8 @@ def index():
     # Please find en bedre måde at fjerne quotes og html encodet newline(%0A)
     user_id = user_id.replace('\"', '').rstrip()
     user = session.get("http://user-service:5100/api/users/{}".format(user_id), headers=headers)
-    r = session.delete("http://user-service:5100/api/users/{}".format(user_id), headers=headers)
-    return render_template('index.html', user=user.json(), id=user_id, delete=r.text)
+
+    return render_template('index.html', user=user.json(), id=user_id, delete="no")
 
 @app.route('/addUser', methods=['GET', 'POST'])
 def addUser():
@@ -26,6 +26,25 @@ def addUser():
     json["access_token"] = "sudo giv mig adgang"
     r = session.post("http://user-service:5100/api/users", json=json, headers=headers)
     return render_template('index.html', text=r)
+
+@app.route('/patchUser', methods=['GET', 'POST', 'PATCH'])
+def patchUser():
+    user_id = session.get("http://user-service:5100/api/users/{}".format(username), headers=headers).text
+    # Please find en bedre måde at fjerne quotes og html encodet newline(%0A)
+    user_id = user_id.replace('\"', '').rstrip()
+
+    json = {}
+    json["userId"] = user_id
+    json["operation"] = False
+    json["username"] = "guderbogi"
+    json["password"] = "nytpassword"
+    json["device"] = "pissefeddeviceid!"
+    json["rule"] = "DetErIkkeMigDerLaverReglerne"
+
+
+
+    user = session.get("http://user-service:5100/api/users/{}".format(user_id), headers=headers)
+    return render_template('index.html', text=user.json())
 
 
 if __name__ == "__main__":

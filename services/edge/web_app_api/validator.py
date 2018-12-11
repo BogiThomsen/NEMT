df14ed224cd127416120e258239cc11285873873
Kasper Røgen <kasperhangard@hotmail.com>
2018-12-11 15:08:25 +0100

    update to validator

--- services/edge/web_app_api/validator.py
+++ services/edge/web_app_api/validator.py
@@ -14,39 +14,62 @@ def is_alphanumeric_or_whitespace(string):
 
 
 def is_only_expected_data(recieved_dictionary, expected_dictionary):
+
     for key in expected_dictionary:
-        if key not in list(recieved_dictionary.keys()):
+        if key not in recieved_dictionary.keys():
             return False
 
     for key in list(recieved_dictionary.keys()):
         if key not in expected_dictionary:
-            return False;
-    return True;
+            return False
+    return True
 
 
 def validate_user_request(request):
-
     if is_only_expected_data(request.json, ["username", "password"]) == False:
         return False
 
     username = request.json["username"]
     password = request.json["password"]
-    
+
     return is_alphanumeric(username) and is_alphanumeric(password)
 
 
 def validate_device_request(request):
+
+    if is_only_expected_data(request.json, ["prettyName"]) == False:
+        return False
+
     prettyName = request.json["prettyName"]
+
     return is_alphanumeric_or_whitespace(prettyName)
 
 
 def validate_sensor_request(request):
+    if is_only_expected_data(request.json, ["prettyName", "public", "accessTokens"]) == False:
+        return False
+
     prettyname = request.json["prettyName"]
     public = request.json["public"]
     accesstokens = request.json["accessTokens"]
 
     for token in accesstokens:
         if(is_alphanumeric(token) == False):
-            return False;
+            return False
+
+    return is_alphanumeric_or_whitespace(prettyname) and isinstance(public, bool)
+
+
+def validate_action_request(request):
+    if is_only_expected_data(request.json, ["prettyName", "public", "accessTokens"]) == False:
+        return False
+
+    prettyname = request.json["prettyName"]
+    public = request.json["public"]
+    accesstokens = request.json["accessTokens"]
+
+    for token in accesstokens:
+        if(is_alphanumeric(token) == False):
+            return False
 
     return is_alphanumeric_or_whitespace(prettyname) and isinstance(public, bool)

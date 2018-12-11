0495c2b22b4778e300d9db2a382305e143da0d8e
Christian Knudsen <cakn15@student.aau.dk>
2018-12-11 14:50:26 +0100

    Core User Service should roughly be done. Needs testing.

--- services/core/user/app/user_api.py
+++ services/core/user/app/user_api.py
@@ -1,5 +1,5 @@
-from flask import jsonify, request, json
-import requests
+from flask import jsonify, request, json, make_response
+import requests, hashlib, uuid
 
 users = []
 user1 = {
@@ -14,7 +14,7 @@ users.append(user1)
 def add_user():
     user = {
         "username" : request.json["username"],
-        "password" : request.json["password"],
+        "password" : hashPassword(request.json["password"]),
         "access_token" : request.json["access_token"]
     }
     r = requests.post("http://user-access:5200/v1/users", json=user)
@@ -26,6 +26,7 @@ def delete_user(id):
 
 def get_user(id):
     r = requests.get("http://user-access:5200/v1/users/{}".format(id))
+    r["password"] = None
     return r.json()
 
 def get_user_id(username):
@@ -34,4 +35,31 @@ def get_user_id(username):
 
 def patch_user(id):
     r = requests.patch("http://user-access:5200/v1/users/{}".format(id), json=request.json)
+
+def authenticate_user():
+    login = {
+        "username" : request.json["username"],
+        "password" : request.json["password"]
+    }
+    userId = requests.get("http://user-access:5200/v1/users/getId/{}".format(login["username"])).text
+    userId = userId.replace('\"', '').rstrip()
+    user = requests.get("http://user-access:5200/v1/users/{}".format(userId), json=login).json()
+    if checkPassword(login["password"], user["password"]):
+        user["password"] = None
+        return user
+    else:
+        return make_response(401)
+
+def hashPassword(password):
+    foo = uuid.uuid4().hex
+    return hashlib.sha256(foo.encode() + password.encode()).hexdigest() + ':' + foo
+
+def checkPassword(userPassword, hashedPassword):
+    password, foo = hashedPassword.split(':')
+    return password == hashlib.sha256(foo.encode() + userPassword.encode()).hexdigest()
+
+#Tag et device id og et device liste og et user id
+#Check device id not in list
+#if true call user/id med patch request. Den skal have en operation, og en entry i otherdevice.
+#Operation og device skal i en json body.
 

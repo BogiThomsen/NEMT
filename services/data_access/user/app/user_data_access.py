22819bd070ae3a98ff60ec2f76ff195c46605d39
Unknown <andreascml@gmail.com>
2018-12-11 14:25:00 +0100

    fixed patch, it now uses arrays for inputs instead of strings

--- services/data_access/user/app/user_data_access.py
+++ services/data_access/user/app/user_data_access.py
@@ -20,7 +20,9 @@ def post_user():
             "access_token": request.json["access_token"]
         }
         user_db.insert_one(new_user)
-        return "user: {}, was added".format(username)
+        user = user_db.find_one({"username": username})
+        user["_id"] = str(user["_id"])
+        return user
 
 
 def delete_user(id):
@@ -88,21 +90,28 @@ def patch_user(id):
     lists = {"device", "rule", "grouping", "other_devices"}
     lists_dict = list_dict()
     user_db = connect_to_db()
+    user = user_db.find_one({"_id": ObjectId(id)})
     for val in lists:
         if val in request.json:
             if request.json["operation"]:
-                user_db.update_one({"_id": ObjectId(id)},
-                                   {"$pull": {lists_dict[val]: request.json[val]}})
+                for item in request.json[val]:
+                    user_db.update_one({"_id": ObjectId(id)},
+                                       {"$pull": {lists_dict[val]: item}})
             if request.json["operation"] == False:
-                user_db.update_one({"_id": ObjectId(id)},
-                                   {"$addToSet": {lists_dict[val]: request.json[val]}})
-    for val in strings:
-        if val in request.json:
-            if ((val == "username") and (user_db.count_documents({"username": request.json[val]})) > 0):
-                return make_response("username already exists", 400)
-            else:
-                user_db.update_one({"_id": ObjectId(id)},
-                                   {"$set": {strings_dict[val]: request.json[val]}})
+                for item in request.json[val]:
+                    user_db.update_one({"_id": ObjectId(id)},
+                                       {"$addToSet": {lists_dict[val]: item}})
+    if request.json["operation"] == False:
+        for val in strings:
+            if val in request.json:
+                if ((val == "username") and (user_db.count_documents({"username": request.json[val]})) > 0):
+                    if request.json[val] == user["username"]:
+                        continue
+                    else:
+                        return make_response("username already exists", 400)
+                else:
+                    user_db.update_one({"_id": ObjectId(id)},
+                                       {"$set": {strings_dict[val]: request.json[val]}})
 
 
 def string_dict():
@@ -117,7 +126,7 @@ def list_dict():
         "device": "devices",
         "rule": "rules",
         "grouping": "groupings",
-        "other_device": "other_devices"
+        "other_devices": "other_devices"
     }
     return dict
 

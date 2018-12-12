77853bf2d4ee202ea842e90ebb1f953059822c98
Bogi <bogi1511@gmail.com>
2018-12-12 20:38:38 +0100

    removed devices from index.html template

--- services/core/device/app/main.py
+++ services/core/device/app/main.py
@@ -6,7 +6,7 @@ app.add_api('swagger.yml')
 
 @app.route('/')
 def index():
-   return render_template('index.html', devices=devices)
+   return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port='5400')

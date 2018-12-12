eff085bb6ed4670868a0589634c301070d1a28ea
Bogi <bogi1511@gmail.com>
2018-12-12 20:18:57 +0100

    updated swagger and removed unnessecary methods

--- services/core/device/app/device_api.py
+++ services/core/device/app/device_api.py
@@ -3,26 +3,32 @@ import requests
 
 ### Data Access Endpoints
 
-def add_device():
+def add_device(userid):
     device = {
         "name" : request.json["name"],
         "token" : request.json["token"]
     }
-    r = requests.post("http://device-access:5500/v1/devices", json=device)
-    return jsonify({'response': r.json()})
+    created_device = requests.post("http://device-access:5500/v1/devices", json=device).json()
+    json = {
+        "operation":"add",
+        "device":created_device["_id"]
+    }
+    user_response = requests.patch("http://user-service:5100/v1/users/{}".format(userid), json=json)
+    return created_device
 
-def delete_device(id):
-    r = requests.delete("http://device-access:5500/v1/devices/{}".format(id))
+def delete_device(userid, deviceid):
+    device_response = requests.delete("http://device-access:5500/v1/devices/{}".format(deviceid))
+    json = {
+        "operation":"remove",
+        "device":deviceid
+    }
+    user_response = requests.patch("http://user-service:5100/v1/users/{}".format(userid), json=json)
+    return device_response.text
+
+def get_device(userid, deviceid):
+    device = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid)).json()
+    return device
+
+def patch_device(userid, deviceid):
+    r = requests.patch("http://device-access:5500/v1/devices/{}".format(deviceid), json=request.json)
     return r.text
-
-def get_device(id):
-    r = requests.get("http://device-access:5500/v1/devices/{}".format(id))
-    return r.json()
-
-# def get_device_id(devicename):
-#     r = requests.get("http://device-access:5500/v1/devices/getId/{}".format(devicename))
-#     return r.text
-
-def patch_device(id):
-    r = requests.patch("http://device-access:5500/v1/devices/{}".format(id), json=request.json)
-
--- services/core/device/app/main.py
+++ services/core/device/app/main.py
@@ -6,25 +6,10 @@ app = connexion.App(__name__, specification_dir = './')
 app.add_api('swagger.yml')
 
 
-#@app.route('/')
-#def index():
-#    return render_template('index.html', devices=devices)
+@app.route('/')
+def index():
+   return render_template('index.html', devices=devices)
 
-@app.route('/devices/String:device_name')
-def get_device_id_from_device_name(device_name):
-    device = request.json(device_name)
-    device_id = data_access.get_device_id_by_name(device)
-    return device_id
-
-@app.route('/devices/String:device_id')
-def getDevice(device_id):
-    id = request.json(device_id)
-    return data_access.get_device(id)
-
-@app.route('/devices/String:device_id')
-def deleteDevice(device_id):
-    id = request.json(device_id)
-    data_access.delete_device(id)
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port='5400')

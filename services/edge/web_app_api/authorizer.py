8d9b858afee5d3633a29af7291a2ae86c13bafd5
Jens Burla Johansen <jbjo15@student.aau.dk>
2018-12-12 17:52:10 +0100

    Changed authorizer return values to strings instead of bools

--- services/edge/web_app_api/authorizer.py
+++ services/edge/web_app_api/authorizer.py
@@ -1,10 +1,9 @@
 import requests
 
-
 def authorize(access_token):
     r = requests.post('http://user-service:5100/v1/users/authorize', data={'accessToken': access_token})
 
     if r.status_code == 200:
-        return True
-    elif r.status_code == 400:
-        return False
+        return ''
+    elif r.status_code == 401:
+        return r.get_json().error

7c19924cd4c2927bee117d1181cf9db70ad779f8
Bogi <bogi1511@gmail.com>
2018-12-12 20:20:31 +0100

    removed non-functioning import statement

--- services/core/device/app/main.py
+++ services/core/device/app/main.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template, request
 import connexion
-import services.data_access.device.device_data_access as data_access
 
 app = connexion.App(__name__, specification_dir = './')
 app.add_api('swagger.yml')
 
-
 @app.route('/')
 def index():
    return render_template('index.html', devices=devices)
-
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0', port='5400')

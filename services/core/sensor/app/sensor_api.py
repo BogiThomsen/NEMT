468c210649cc4d71b0c0beb0c39c97de19c36059
Bogi <bogi1511@gmail.com>
2018-12-13 10:51:50 +0100

    something

--- services/core/sensor/app/sensor_api.py
+++ services/core/sensor/app/sensor_api.py
@@ -3,7 +3,7 @@ import requests
 
 ### Data Access Endpoints
 
-def add_sensor(userid, deviceid):
+def add_sensor(userid, devicetoken):
     sensor = {
         "name" : request.json["name"],
         "token" : request.json["token"]
@@ -13,22 +13,22 @@ def add_sensor(userid, deviceid):
         "operation":"add",
         "sensor":created_sensor["name"]+":"+created_sensor["_id"]
     }
-    device_response = requests.patch("http://device-service:5400/v1/users/{0}/devices/{1}".format(userid, deviceid), json=json)
+    device_response = requests.patch("http://device-service:5400/v1/users/{0}/devices/{1}".format(userid, devicetoken), json=json)
     return created_sensor
 
-def delete_sensor(userid, deviceid, sensorid):
+def delete_sensor(userid, devicetoken, sensorid):
     sensor_response = requests.delete("http://sensor-access:5600/v1/sensors/{}".format(sensorid))
     json = {
         "operation":"remove",
         "sensor":sensorid
     }
-    device_response = requests.patch("http://device-service:5400/v1/users/{0}/devices/{1}".format(userid, deviceid), json=json)
+    device_response = requests.patch("http://device-service:5400/v1/users/{0}/devices/{1}".format(userid, devicetoken), json=json)
     return sensor_response.text
 
-def get_sensor(userid, deviceid, sensorid):
+def get_sensor(userid, devicetoken, sensorid):
     sensor = requests.get("http://sensor-access:5600/v1/sensors/{}".format(sensorid)).json()
     return sensor
 
-def patch_sensor(userid, deviceid, sensorid):
+def patch_sensor(userid, devicetoken, sensorid):
     r = requests.patch("http://sensor-access:5600/v1/sensors/{}".format(sensorid), json=request.json)
     return r.text

76c3e9b03e4c2a596a41aeccdd582e785e5bedf5
Bogi <bogi1511@gmail.com>
2018-12-13 11:24:00 +0100

    updated parameters

--- services/core/device/app/device_api.py
+++ services/core/device/app/device_api.py
@@ -34,14 +34,14 @@ def patch_device(userid, deviceid):
     r = requests.patch("http://device-access:5500/v1/devices/{}".format(deviceid), json=request.json)
     return r.text
 
-def patch_sensor_values(deviceid, sensorname):
+def patch_sensor_values(deviceid, sensorid):
     device = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid)).json()
     sensor_patch = request.json()
 
     #Iterate thorugh the list of sensors and match sensorid
     for x in device["sensors"]:
         sensordata=x.split(':')
-        if sensordata[0] == sensorname:
+        if sensordata[0] == sensorid:
             sensor_id = sensordata[1]
             break
     requests.patch("http://sensor-service:5800/v1/devices/{0}/sensors/{1}".format(deviceid, sensor_id), json=sensor_patch)
@@ -49,14 +49,14 @@ def patch_sensor_values(deviceid, sensorname):
     #When sensor is found, make get request for sensor
     #When sensor is retrieved, update sensor with body and make patch request
 
-def patch_action_values(deviceid, actionname):
+def patch_action_values(deviceid, actionid):
     device = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid)).json()
     action_patch = request.json()
 
     #Iterate thorugh the list of actions and match actionid
     for x in device["actions"]:
         actiondata=x.split(':')
-        if actiondata[0] == actionname:
+        if actiondata[0] == actionid:
             action_id = actiondata[1]
             break
     requests.patch("http://action-service:5900/v1/devices/{0}/actions/{1}".format(deviceid, action_id), json=action_patch)

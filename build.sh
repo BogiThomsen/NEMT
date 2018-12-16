67103eb58469676b6932314c994b955caa39f0da
Bogi <bogi1511@gmail.com>
2018-12-16 13:44:48 +0100

    testing scaling

--- build.sh
+++ build.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
 
-cd services/core/user
-docker-compose build --no-cache
-cd ..
-cd device
-docker-compose build --no-cache
-cd ..
-cd sensor
-docker-compose build --no-cache
-cd ..
-cd action
-docker-compose build --no-cache
-cd ..
-cd ..
-cd data_access/user
-docker-compose build --no-cache
-cd ..
-cd device
-docker-compose build --no-cache
-cd ..
-cd sensors
-docker-compose build --no-cache
-cd ..
-cd actions
-docker-compose build --no-cache
-cd ..
-cd ..
-cd edge/web_app_api
-docker-compose build --no-cache
-cd ..
-cd device_api
+cd services
 docker-compose build --no-cache
 cd ..
 cd web_app
 docker-compose build
-cd ..
-cd ..
-cd ..
+
 
--- up.sh
+++ up.sh
@@ -2,37 +2,8 @@
 
 docker network create --driver bridge platform
 
-cd services/edge/web_app_api
+cd services
 docker-compose up -d
 cd ..
 cd web_app
-docker-compose up -d
-cd ..
-cd device_api
-docker-compose up -d
-cd ..
-cd ..
-cd core/user
-docker-compose up -d
-cd ..
-cd device
-docker-compose up -d
-cd ..
-cd sensor
-docker-compose up -d
-cd ..
-cd action
-docker-compose up -d
-cd ..
-cd ..
-cd data_access/user
-docker-compose up -d
-cd ..
-cd device
-docker-compose up -d
-cd ..
-cd sensors
-docker-compose up -d
-cd ..
-cd actions
 docker-compose up -d

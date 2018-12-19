ac19341af30c49bd3246b5993b8ffb65f1620af0
Bogi <bogi1511@gmail.com>
2018-12-19 18:35:56 +0100

    updated documentation in the code, for core

--- services/core/device/app/device_api.py
+++ services/core/device/app/device_api.py
@@ -6,6 +6,17 @@ import sys
 ### Data Access Endpoints
 
 def add_device(userid):
+    """Adds a device to the database
+
+    Args:
+        userid (string): the id of the user
+        json (json): json object containing an accestoken and the information to create a device. 
+        
+
+    Returns:
+        the device as a json object.
+
+    """
     device = request.json
     
     device_response = requests.post("http://device-access:5500/v1/devices", json=device)
@@ -24,10 +35,34 @@ def add_device(userid):
         return make_response(device_response.content, device_response.status_code)
 
 def get_devices(userid):
+    """Fetches a list of devices from the database, based on ids.
+
+    Args:
+        json (json): json object containing an accestoken and an array of ids of the Devices
+
+    Returns:
+        the Devices.
+
+    """
     device_response = requests.get("http://device-access:5500/v1/devices", json=request.json)
     return make_response(device_response.content, device_response.status_code)
 
 def delete_device(userid, deviceid):
+    """deletes a device from the database, based on an id. 
+        Also deletes all the actions and sensors linked to that device, 
+        and removes the device from the users deviceList.
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device 
+        json (json): json object containing an accestoken
+
+        
+
+    Returns:
+        200 if the device was deleted, 400 if not.
+
+    """
     device = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid)).json()
     if 'sensors' in device:
         for x in device["sensors"]:
@@ -53,14 +88,49 @@ def delete_device(userid, deviceid):
     return make_response(device_response.content, device_response.status_code)
 
 def get_device(userid, deviceid):
+    """Fetches a device from the database, based on an id.
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device 
+        json (json): json object containing an accestoken
+        
+
+    Returns:
+        the device as a json object.
+
+    """
     device_response = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid))
     return make_response(device_response.content, device_response.status_code)
 
 def patch_device(userid, deviceid):
+    """updates a device information.
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device 
+        json (json): json object containing an accestoken and the information to be updated on the device.
+        
+
+    Returns:
+        the device as a json object.
+
+    """
     device_response = requests.patch("http://device-access:5500/v1/devices/{}".format(deviceid), json=request.json)
     return make_response(device_response.content, device_response.status_code)
 
 def patch_sensor_values(deviceid, sensorid):
+    """Request and update of a sensor, based on device token and sensor name.
+
+    Args:
+        deviceid (string): the token of the device
+        sensorid (string): the name of the sensor 
+        
+
+    Returns:
+        200 if sensor is updated, 402 if not.
+
+    """
     device = requests.get("http://device-access:5500/v1/devices/{}".format(deviceid)).json()
     sensor_patch = request.json
 
--- services/core/sensor/app/sensor_api.py
+++ services/core/sensor/app/sensor_api.py
@@ -4,6 +4,17 @@ import requests
 ### Data Access Endpoints
 
 def add_sensor(userid, deviceid):
+    """Adds a sensor to the database
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device
+        json (json): json object containing an accestoken and the information to create a sensor. 
+        
+
+    Returns:
+        the sensor as a json object.
+    """
     sensor = request.json
     sensor_response = requests.post("http://sensor-access:5600/v1/sensors", json=sensor)
     created_sensor = sensor_response.json()
@@ -17,10 +28,33 @@ def add_sensor(userid, deviceid):
     return make_response(sensor_response.content, sensor_response.status_code)
 
 def get_sensors(userid, deviceid):
+    """Fetches a list of sensors from the database
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device
+        json (json): json object containing an accestoken and an array of ids of the sensors. 
+        
+
+    Returns:
+        the sensors in an array as a json object.
+    """
     sensor_response = requests.get("http://sensor-access:5600/v1/sensors", json=request.json)
     return make_response(sensor_response.content, sensor_response.status_code)
 
 def delete_sensor(userid, deviceid, sensorid):
+    """Deletes a sensor from the database
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device
+        sensorid (string): the id of the sensor
+        json (json): json object containing an accestoken 
+        
+
+    Returns:
+        200 response if the sensor is deleted, 402 if not.
+    """
     sensor_response = requests.get("http://sensor-access:5600/v1/sensors/{}".format(sensorid))
     sensor = sensor_response.json()
     sensor_list = []
@@ -34,13 +68,47 @@ def delete_sensor(userid, deviceid, sensorid):
     return make_response(sensor_response.content, sensor_response.status_code)
 
 def get_sensor(userid, deviceid, sensorid):
+    """Fetches a sensor from the database
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device
+        sensorid (string): the id of the sensor
+        json (json): json object containing an accestoken
+        
+
+    Returns:
+        the sensor as a json object.
+    """
     sensor_response = requests.get("http://sensor-access:5600/v1/sensors/{}".format(sensorid))
     return make_response(sensor_response.content, sensor_response.status_code)
 
 def patch_sensor(userid, deviceid, sensorid):
+    """Updates a sensor
+
+    Args:
+        userid (string): the id of the user
+        deviceid (string): the id of the device
+        sensorid (string): the id of the sensor
+        json (json): json object containing an accestoken and the information to update a sensor. 
+        
+
+    Returns:
+        the updated sensor as a json object.
+    """
     sensor_response = requests.patch("http://sensor-access:5600/v1/sensors/{}".format(sensorid), json=request.json)
     return make_response(sensor_response.content, sensor_response.status_code)
 
 def device_patch_sensor(deviceid, sensorid):
+    """Updates a sensor value
+
+    Args:
+        userid (string): the name of the user
+        deviceid (string): the token of the device
+        
+
+    Returns:
+        200 if the sensor is updated, 402 if not.
+    """
     sensor_response = requests.patch("http://sensor-access:5600/v1/sensors/{}".format(sensorid), json=request.json)
     return make_response(sensor_response.content, sensor_response.status_code)
